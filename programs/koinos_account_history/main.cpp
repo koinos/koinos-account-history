2669191639fb8d3b0c07d493f9eb2e0fc51d364a
Steve Gerbino <steve@gerbino.co>
2023-10-02 17:37:49 -0400

    Add log-datetime option

--- programs/koinos_account_history/main.cpp
+++ programs/koinos_account_history/main.cpp
@@ -43,7 +43,9 @@
 #define LOG_DIR_OPTION                 "log-dir"
 #define LOG_DIR_DEFAULT                ""
 #define LOG_COLOR_OPTION               "log-color"
-#define LOG_COLOR_DEFAULT              false
+#define LOG_COLOR_DEFAULT              true
+#define LOG_DATETIME_OPTION            "log-datetime"
+#define LOG_DATETIME_DEFAULT           true
 #define INSTANCE_ID_OPTION             "instance-id"
 #define STATEDIR_OPTION                "statedir"
 #define STATEDIR_DEFAULT               "db"
@@ -187,7 +189,8 @@ int main( int argc, char** argv )
          (RESET_OPTION                     , program_options::value< bool >(), "Reset the database")
          (FORK_ALGORITHM_OPTION        ",f", program_options::value< std::string >(), "The fork resolution algorithm to use. Can be 'fifo', 'pob', or 'block-time'. (Default: 'fifo')")
          (LOG_DIR_OPTION                   , program_options::value< std::string >(), "The logging directory")
-         (LOG_COLOR_OPTION                 , program_options::value< bool >(), "Log color toggle");
+         (LOG_COLOR_OPTION                 , program_options::value< bool >(), "Log color toggle")
+         (LOG_DATETIME_OPTION              , program_options::value< bool >(), "Log datetime on console toggle");
 
       program_options::variables_map args;
       program_options::store( program_options::parse_command_line( argc, argv, options ), args );
@@ -231,6 +234,7 @@ int main( int argc, char** argv )
       auto log_level             = util::get_option< std::string >( LOG_LEVEL_OPTION, LOG_LEVEL_DEFAULT, args, account_history_config, global_config );
       auto log_dir               = util::get_option< std::string >( LOG_DIR_OPTION, LOG_DIR_DEFAULT, args, account_history_config, global_config );
       auto log_color             = util::get_option< bool >( LOG_COLOR_OPTION, LOG_COLOR_DEFAULT, args, account_history_config, global_config );
+      auto log_datetime          = util::get_option< bool >( LOG_DATETIME_OPTION, LOG_DATETIME_DEFAULT, args, account_history_config, global_config );
       auto instance_id           = util::get_option< std::string >( INSTANCE_ID_OPTION, util::random_alphanumeric( 5 ), args, account_history_config, global_config );
       auto jobs                  = util::get_option< uint64_t >( JOBS_OPTION, std::max( JOBS_DEFAULT, uint64_t( std::thread::hardware_concurrency() ) ), args, account_history_config, global_config );
       auto statedir              = std::filesystem::path( util::get_option< std::string >( STATEDIR_OPTION, STATEDIR_DEFAULT, args, account_history_config, global_config ) );
@@ -246,7 +250,7 @@ int main( int argc, char** argv )
             logdir_path = basedir / util::service::account_history / *logdir_path;
       }
 
-      koinos::initialize_logging( util::service::account_history, instance_id, log_level, logdir_path, log_color );
+      koinos::initialize_logging( util::service::account_history, instance_id, log_level, logdir_path, log_color, log_datetime );
 
       LOG(info) << version_string();
 
